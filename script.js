506439d0801f2dab4a93be5ef3947b653c5ac0f2
ardamoin <ardamoin2@gmail.com>
2022-11-03 21:15:24 -0400

    Add operate function

--- script.js
+++ script.js
@@ -31,3 +31,19 @@ function divide(numerator, ...args) {
         return total / current;
     }, numerator);
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+            
+        case '/':
+            return divide(num1, num2);
+    }
+}
