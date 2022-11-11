b5841cdc4e2f51ed96eb9f6a5c8500d0c97c9c18
ardamoin <ardamoin2@gmail.com>
2022-11-11 00:43:05 -0500

    Optimize event listeners

--- script.js
+++ script.js
@@ -66,20 +66,34 @@ function myEval(expression) {
     return total;
 }
 
+function isNumeric(str) {
+    if (typeof str != "string") return false
+    return !isNaN(str) && !isNaN(parseFloat(str))
+}
+
+function inputLogic(input) {
+    
+}
+
+
+
+
 window.addEventListener('keydown', function(e) {
     e.key = (e.key === "Shift") ? "" : e.key; // to ignore shift key
 
-    if (!reset && keys.includes(e.key)) {
+    if (!reset && keys.includes(e.key) && isNumeric(displayText.textContent + e.key)) {
         displayText.textContent += e.key;
     } else if (operatorKeys.includes(e.key) && !reset) {
         reset = true;
 
         if (calculationExp.test(calculationText.textContent)) {
             calculationText.textContent = myEval(calculationText.textContent + displayText.textContent) + " " + e.key + " ";
-        } else {
+        } else if (displayText.textContent !== "") {
             calculationText.textContent += displayText.textContent + " " + e.key + " ";
         }
 
+    } else if (operatorKeys.includes(e.key) && reset) {
+        calculationText.textContent = calculationText.textContent.slice(0,-2) + e.key + " ";
     } else if(reset && keys.includes(e.key)) {
         displayText.textContent = e.key;
         reset = false;
@@ -101,17 +115,19 @@ window.addEventListener('keydown', function(e) {
 numsAndOperators.forEach(item => {
     item.addEventListener('mousedown', () => {
         console.log(item);
-        if (!reset && keys.includes(item.textContent)) {
+        if (!reset && keys.includes(item.textContent) && isNumeric(displayText.textContent + item.textContent)) {
             displayText.textContent += item.textContent;
         } else if (operatorKeys.includes(item.textContent) && !reset) {
             reset = true;
     
             if (calculationExp.test(calculationText.textContent)) {
                 calculationText.textContent = myEval(calculationText.textContent + displayText.textContent) + " " + item.textContent + " ";
-            } else {
+            } else if (displayText.textContent !== "") {
                 calculationText.textContent += displayText.textContent + " " + item.textContent + " ";
             }
     
+        } else if (operatorKeys.includes(e.key) && reset) {
+            calculationText.textContent = calculationText.textContent.slice(0,-2) + item.textContent + " ";
         } else if(reset && keys.includes(item.textContent)) {
             displayText.textContent = item.textContent;
             reset = false;
